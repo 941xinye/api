edb9f143e4b6c69ee552251dafd64f043ffe807b
yexin <yex@befound.cn>
2017-01-16 14:20:29 +0800

    .

--- basic/modules/ful/controllers/UserController.php
+++ basic/modules/ful/controllers/UserController.php
@@ -9,9 +9,9 @@ class UserController extends BaseController
     public function actionInfo()
     {
         $memId = Yii::$app->user->identity->mem_id;
-        $this->return['data'] = Members::findOne($memId);
-        $this->return['state'] = 1;
-        $this->return['message'] = 'ok';
+        $this->return->data = Members::findOne($memId);
+        $this->return->state = 1;
+        $this->return->message = 'ok';
         return $this->return;
     }
 
