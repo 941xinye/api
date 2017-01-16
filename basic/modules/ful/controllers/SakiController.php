94e14acbd2b5a202431f014e3c5c99a274d2cc0d
yexin <yex@befound.cn>
2017-01-16 15:59:15 +0800

    .

--- basic/modules/ful/controllers/SakiController.php
+++ basic/modules/ful/controllers/SakiController.php
@@ -1,7 +1,9 @@
 <?php
 namespace app\modules\ful\controllers;
+use app\backend\libs\Curl;
 use app\models\Saki;
 use Yii;
+use yii\helpers\Url;
 
 //use yii\rest\UrlRule;
 class SakiController extends BaseController
@@ -18,4 +20,8 @@ class SakiController extends BaseController
         return $this->return;
     }
 
+    public function actionTt(){
+        $curl = new Curl();
+        return $curl->curl_get(Url::to(['/api/saki/opening']),['access-token: feafc705c0ea25457467df804040f55f']);
+    }
 }
