312cbeb3a8422e43a3342c9e99c570c21ce40598
yexin <yex@befound.cn>
2017-01-16 16:33:58 +0800

    .

--- basic/compents/CQueryParamAuth.php
+++ basic/compents/CQueryParamAuth.php
@@ -16,7 +16,6 @@ class CQueryParamAuth extends \yii\filters\auth\QueryParamAuth
     public function authenticate($user, $request, $response)
     {
         $headers = $request->headers;
-        print_r($headers);exit;
         $accessToken = $headers->get($this->tokenParam);
         if (is_string($accessToken)) {
             $identity = $user->loginByAccessToken($accessToken, get_class($this));
